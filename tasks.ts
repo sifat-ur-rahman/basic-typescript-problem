36fbe06e60ec038ae72dbdf837bda76380def52c
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-09 15:49:11 +0600

    add-p1

---
+++ tasks.ts
@@ -0,0 +1,15 @@
+//Problem 1:
+
+function getSquareLength(params:string|number) {
+    if (typeof params === "number") {
+        return params*params
+    } else  {
+        return params.length
+    }
+}
+
+
+//Problem 2:
+
+
+
