b391a8b133ab79c2689def1f3cf1762e3598a26a
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-09 15:54:33 +0600

    add-p2

--- tasks.ts
+++ tasks.ts
@@ -11,5 +11,17 @@ function getSquareLength(params:string|number) {
 
 //Problem 2:
 
+interface Person {
+    name: string;
+    email: string;
+    address?:{
+        city: string;
+        street: string;
+    };
+    phone?:number
+}
 
+function getAddressCity(person:Person) {
+    return person.address? person?.address?.city : 'There is no address'
+}
 
